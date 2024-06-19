d8e8937e9efcd85f5a931775d8028111c8d5bf94
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-06-19 17:44:32 +0200

    Project reset

--- src/main/java/com/jwt/auth/AuthApplication.java
+++ src/main/java/com/jwt/auth/AuthApplication.java
@@ -1,13 +1,13 @@
-package com.v.security;
+package com.jwt.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class AuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 
 }
--- src/test/java/com/jwt/auth/AuthApplicationTests.java
+++ src/test/java/com/jwt/auth/AuthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.v.security;
+package com.jwt.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecurityApplicationTests {
+class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
